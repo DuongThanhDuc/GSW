9611f34aa863e67190fa20d883820395e7b31702
NguyenThaiLongVu <142371425+NguyenThaiLongVu@users.noreply.github.com>
2025-07-04 13:31:35 +0700

    [add] outline, view list game in homepage, gamedetal ...

---
+++ src/app/modules/dashboard/components/special-offers/special-offers.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { GameService } from 'src/app/core/services/game.service';
+import { GameInfor } from 'src/app/core/models/db.model';
+
+@Component({
+  selector: 'app-special-offers',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './special-offers.component.html',
+  styleUrls: ['./special-offers.component.css']
+})
+export class SpecialOffersComponent implements OnInit{
+  constructor(private service : GameService){}
+  public listGame : GameInfor[]=[];
+  ngOnInit(): void {
+ this.service.getListGame().subscribe((data)=>{
+  this.listGame = data.data;
+ })
+  }
+  @Input() games: any[] = [];
+
+}
